b229d062fabc657f5ab209837d6e68a7ed659c13
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2024-09-13 08:23:23 +0700

    Praktikum 2.2 Done

--- database/migrations/2024_09_11_141708_create_t_penjualan_table.php
+++ database/migrations/2024_09_11_141708_create_t_penjualan_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
             $table->id('penjualan_id');
             $table->unsignedBigInteger('user_id')->index();
             $table->string('pembeli', 50);
-            $table->string('penjualan_kode', 50)->unique();
+            $table->string('penjualan_kode', 20)->unique();
             $table->dateTime('penjualan_tanggal');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
