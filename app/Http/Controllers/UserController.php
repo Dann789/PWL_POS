b7932dbff4e87acff54dea3b8795c4701fe22a67
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2024-09-29 14:15:45 +0700

    JS5 Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -204,12 +204,19 @@ class UserController extends Controller
 
             $activeMenu = 'user'; //Set menu yang sedang aktif
 
-            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+            $level = LevelModel::all(); // ambil data level untuk filter level
+
+            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
         }
 
         public function list(Request $request) {
             $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+            // Filter data user berdasarkan level_id
+            if($request->level_id) {
+                $users->where('level_id', $request->level_id);
+            }
 
             return DataTables::of($users)
             // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
